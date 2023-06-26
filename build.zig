b1a98b85185a0a59f5de2ef3ba453c435dacfc71
jobat <jobat@toolbelt-games.com>
2023-06-26 16:06:11 +0100

    added a build.zig for lua

---
+++ build.zig
@@ -0,0 +1,52 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const liblua = buildLibLua(b, target, optimize);
+
+    const exe = b.addExecutable(.{
+        .name = "lua",
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.linkLibC();
+    exe.linkLibrary(liblua);
+    exe.addCSourceFile("lua.c", &.{});
+    b.installArtifact(exe);
+
+    var lib_step = b.step("lib", "emit liblua.a");
+    var emit_liblua = b.addInstallArtifact(liblua);
+    lib_step.dependOn(&emit_liblua.step);
+}
+
+pub fn buildLibLua(b: *std.Build, target: std.zig.CrossTarget, optimize: std.builtin.OptimizeMode) *std.Build.CompileStep {
+    const lua = b.addStaticLibrary(.{
+        .name = "lua",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    for (PresetModules.get(.core)) |module| {
+        lua.addCSourceFile(std.mem.concat(b.allocator, u8, &.{ @tagName(module), ".c" }) catch @panic("OOM"), &.{});
+    }
+    for (PresetModules.get(.lib)) |module| {
+        lua.addCSourceFile(std.mem.concat(b.allocator, u8, &.{ @tagName(module), ".c" }) catch @panic("OOM"), &.{});
+    }
+
+    lua.linkLibC();
+
+    return lua;
+}
+
+const LuaPreset = enum {
+    core,
+    lib,
+};
+
+const PresetModules = std.EnumArray(LuaPreset, []const LuaModule).init(.{
+    .core = &.{ .lapi, .lcode, .lctype, .ldebug, .ldo, .ldump, .lfunc, .lgc, .llex, .lmem, .lobject, .lopcodes, .lparser, .lstate, .lstring, .ltable, .ltm, .lundump, .lvm, .lzio },
+    .lib = &.{ .lauxlib, .lbaselib, .lcorolib, .ldblib, .liolib, .lmathlib, .loadlib, .loslib, .lstrlib, .ltablib, .lutf8lib, .linit },
+});
+
+const LuaModule = enum { lapi, lauxlib, lbaselib, lcode, lcorolib, lctype, ldblib, ldebug, ldo, ldump, lfunc, lgc, linit, liolib, llex, lmathlib, lmem, loadlib, lobject, lopcodes, loslib, lparser, lstate, lstring, lstrlib, ltable, ltablib, ltm, lua, luac, lundump, lutf8lib, lvm, lzio };
